ac8a3b57afb9ffe33092caed1071220495316972
Amr Mahmoud Morsy <ammorsy@MOBCORP.INTRT.COM>
2022-05-16 00:52:16 +0200

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+       
+       
     }
 
     /**
@@ -46,7 +47,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        //
+        return User::find($user);
     }
 
     /**
